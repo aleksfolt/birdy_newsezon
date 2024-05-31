68401d8e990778f47d65a1b3bbdec7ae904a2fa6
aleksfolt <122365476+aleksfolt@users.noreply.github.com>
2024-05-31 08:32:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 import telebot
 from telebot import types
